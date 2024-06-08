2c2efd6ff654af8886e0d34b19cd5a1e02ec24fe
Christian Bongard <christian.bongard90@gmail.com>
2024-06-08 11:17:19 +0400

    added the cleaning and formatting files

---
+++ data-cleaning/exchanges.sql
@@ -0,0 +1,8 @@
+-- Null value for finalised_at: create a boolean value to indicate if the exchange is finalised or not
+-- Date present: represents the date the exchange was finalised
+-- Date not present: the date was not finalised
+
+-- Null values in capacity: remove
+
+-- Null value in country: check if they have a value in the region/city/department column
+-- Populate only if there are values in the region/city/department column
