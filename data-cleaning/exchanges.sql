fe0523221f6197ecd4a46c4892326a6e08251738
Christian Bongard <christian.bongard90@gmail.com>
2024-06-08 11:41:41 +0400

    added confirmation check on exchange

--- data-cleaning/exchanges.sql
+++ data-cleaning/exchanges.sql
@@ -1,8 +1,45 @@
 -- Null value for finalised_at: create a boolean value to indicate if the exchange is finalised or not
 -- Date present: represents the date the exchange was finalised
 -- Date not present: the date was not finalised
+SELECT 
+    conversation_id, finalized_at,
+    IF(finalized_at IS NOT NULL, 1, 0) AS finalized
+FROM `le-wagon-1580.home_exchange.exchanges`;
 
 -- Null values in capacity: remove
+SELECT 
+    COUNT(*)
+FROM `le-wagon-1580.home_exchange.exchanges`
+WHERE capacity IS NULL;
 
 -- Null value in country: check if they have a value in the region/city/department column
 -- Populate only if there are values in the region/city/department column
+
+
+-- Cancelation date
+SELECT 
+    conversation_id, 
+    guest_user_id,
+    host_user_id,
+    creator_id,
+    canceled_at,user_cancellation_id,
+    IF(canceled_at IS NOT NULL, 1, 0) AS cancelled
+FROM `le-wagon-1580.home_exchange.exchanges`
+
+WHERE user_cancellation_id IS NOT NULL
+AND canceled_at IS NULL
+AND creator_id = user_cancellation_id
+LIMIT 500;
+
+-- Data confirmation check
+-- remove any null values in the following: conversation_id, guest_user_id, host_user_id, creator_id, exchange_id
+SELECT 
+ *
+FROM `le-wagon-1580.home_exchange.exchanges`
+
+WHERE 
+  conversation_id IS NULL  
+  OR guest_user_id IS NULL
+  OR host_user_id IS NULL
+  OR exchange_id IS NULL
+  OR creator_id IS NULL
---
+++ data-cleaning/subscriptions.sql
@@ -0,0 +1,5 @@
+-- Null value in country: check if they have a value in the region/city/department column
+-- Populate only if there are values in the region/city/department column
+
+-- Remove the duplicates: similar subscription date, user_id and others
+
